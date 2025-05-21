a63eca06f7009f8027eef7a9fb4c7f3e9255b191
GailZzz0910 <mynameisabegail2015@gmail.com>
2025-05-22 04:23:30 +0800

    test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import Login from "./auth/Login"
+import Login from "./auth/Login.jsx"
 import Home from "./screen/Home";
 import AddCandidate from "./screen/AddCandidate";
 import GetBarangay from "./screen/GetBarangay";
